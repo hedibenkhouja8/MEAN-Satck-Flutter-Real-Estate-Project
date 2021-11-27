0ac2e2cea00bef06c6b0a2aa86f46df187e1f9a5
labidimalek <melek.islem105@gmail.com>
2021-11-27 15:56:04 +0100

    get rent list from file

--- Frontend/Angular-Real-Estate-Project--master/src/app/rent/rent.component.ts
+++ Frontend/Angular-Real-Estate-Project--master/src/app/rent/rent.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { rents } from '../rent-list';
+import { Rent } from '../rent.model';
 
 @Component({
   selector: 'app-rent',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rent.component.css']
 })
 export class RentComponent implements OnInit {
+  public rentList: Rent[] = rents;
 
   constructor() { }
 
