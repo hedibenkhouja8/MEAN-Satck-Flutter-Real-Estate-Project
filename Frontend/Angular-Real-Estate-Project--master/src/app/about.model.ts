8f6c473d792fd4ec42e471fee043dce543ddfb3d
yousseff650 <yousseffarhat818@gmail.com>
2021-11-27 19:28:21 +0100

    creation about details

---
+++ Frontend/Angular-Real-Estate-Project--master/src/app/about.model.ts
@@ -0,0 +1,10 @@
+export interface Agency {
+    id: string;
+    title: string;
+    owner: string;
+    image_owner: string;
+    description: string;
+    image: string;
+    location: string;
+    date_creation:Date;
+  }
--- Frontend/Angular-Real-Estate-Project--master/src/app/about/about-routing.module.ts
+++ Frontend/Angular-Real-Estate-Project--master/src/app/about/about-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about.component';
+import { AboutDetailsComponent } from './about-details/about-details.component';
 
 const routes: Routes = [{ path: '', component: AboutComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [
+    AboutDetailsComponent
+  ]
 })
 export class AboutRoutingModule { }
