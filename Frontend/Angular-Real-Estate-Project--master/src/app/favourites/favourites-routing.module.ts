17bc49800b2db33e6bdb2730ba80bd393ddff8de
oussama-abid <oussamaabid52@gmail.com>
2021-12-06 14:45:28 +0100

    favourites template + buyfav & rentfav components

--- Frontend/Angular-Real-Estate-Project--master/src/app/favourites/favourites-routing.module.ts
+++ Frontend/Angular-Real-Estate-Project--master/src/app/favourites/favourites-routing.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FavouritesComponent } from './favourites.component';
 import { FavouritesBuyComponent } from './favourites-buy/favourites-buy.component';
+import { FavouritesRentComponent } from './favourites-rent/favourites-rent.component';
 
-const routes: Routes = [{ path: '', component: FavouritesComponent }];
+const routes: Routes = [
+  { path: '', component: FavouritesComponent },
+  { path: 'buy-favourites', component: FavouritesBuyComponent },
+  { path: 'rent-favourites', component: FavouritesRentComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
   declarations: [
-    FavouritesBuyComponent
+    FavouritesBuyComponent,
+    FavouritesRentComponent
   ]
 })
 export class FavouritesRoutingModule { }
