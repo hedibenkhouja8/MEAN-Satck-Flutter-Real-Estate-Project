13e6011c531fd7512395d2def3176489d07c1951
labidimalek <melek.islem105@gmail.com>
2021-12-22 13:52:05 +0100

    add crud functions

--- Frontend/Angular-Real-Estate-Project--master/src/app/services/rent.service.ts
+++ Frontend/Angular-Real-Estate-Project--master/src/app/services/rent.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RentService {
+  private apiUrl =
+    'https://my-json-server.typicode.com/houcem-h/public_courses_api/courses';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  /**
+   *Get all rents
+   * @returns Observable<Rent[]>
+   */
+  all() {
+    return this.http.get(this.apiUrl);
+  }
+
+  /**
+   * Get a rent with the given id
+   * @param id : rent id
+   * @returns Observable<Rent>
+   */
+  get(id: string) {
+    return this.http.get(this.apiUrl + '/' + id);
+  }
+
+  /**
+   * Create a new rent
+   * @param course new rent to create
+   */
+  create(rent: any) {
+    return this.http.post(this.apiUrl, rent);
+  }
+
+  /**
+   * Update a rent with the given id
+   * @param id rent id to update
+   * @param rent new course data
+   */
+  update(id: string, rent: any) {
+    return this.http.put(this.apiUrl + '/' + id, rent);
+  }
+
+  /**
+   * Delete a rent with the given id
+   * @param id rent id to delete
+   */
+  delete(id: string) {
+    return this.http.delete(this.apiUrl + '/' + id);
+  }
 }
