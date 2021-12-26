5310912f6bf0601efb0c88fa5a0e6278822386e5
labidimalek <melek.islem105@gmail.com>
2021-12-26 17:17:21 +0100

    get rents with api

--- mobile/lib/model/rent.dart
+++ mobile/lib/model/rent.dart
@@ -1,17 +1,16 @@
-import 'package:flutter/material.dart';
 
 class Rent{
   final String id;
   final String title;
   final String owner;
   final String description;
-  final Image image;
+  final String image;
   final int size;
   final int room_number;
   final String location;
   final num price_per_month;
-  final DateTime start_date_available;
-  final DateTime end_date_available;
+  final String start_date_available;
+  final String end_date_available;
 
   const Rent({
     required this.id,
@@ -26,4 +25,20 @@ class Rent{
     required this.start_date_available,
     required this.end_date_available,
   });
+  factory Rent.fromJson(Map<String, dynamic> json) {
+    return Rent(
+        id: json['id'],
+        title: json['title'],
+        owner: json['owner'],
+        description: json['description'],
+        image: json['image'],
+        size: json['size'],
+        room_number: json['room_number'],
+        location: json['location'],
+        price_per_month: json['price_per_month'],
+        start_date_available: json['start_date_available'],
+        end_date_available: json['end_date_available']
+    );
+  }
+
 }
--- mobile/lib/page/rent_page.dart
+++ mobile/lib/page/rent_page.dart
@@ -1,51 +1,96 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/model/rent.dart';
-import 'package:mobile/utils/rent_list.dart';
 
+//import 'package:mobile/utils/rent_list.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class RentPage extends StatefulWidget {
+  const RentPage({Key? key}) : super(key: key);
+
   @override
   _RentPageState createState() => _RentPageState();
 }
 
 class _RentPageState extends State<RentPage> {
+  final String url =
+      'https://my-json-server.typicode.com/labidimalek/public-rents-api/rents';
+  List<dynamic> _rents = [];
+  bool loading = true;
+
+  @override
+  void initState() {
+    getRents();
+    super.initState();
+  }
+
+  Future<void> getRents() async {
+    var response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final parsedData = jsonDecode(response.body).cast<Map<String, dynamic>>();
+      _rents = parsedData.map<Rent>((json) => Rent.fromJson(json)).toList();
+      setState(() {
+        loading = !loading;
+      });
+    } else {
+      throw Exception('Failed to load rents');
+    }
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
-    //drawer: NavigationDrawerWidget(),
-    appBar: AppBar(
-      title: Text('Rent'),
-      centerTitle: true,
-      backgroundColor: Colors.pink,
-    ),
-    body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemCount: rents.length,
+      //drawer: NavigationDrawerWidget(),
+      appBar: AppBar(
+        title: const Text('Rent'),
+        centerTitle: true,
+        backgroundColor: Colors.pink,
+      ),
+      body: loading ? waitingScreen() : rentsList());
+
+  Widget waitingScreen() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const <Widget>[
+          Text("Loading data ..."),
+          Padding(padding: EdgeInsets.only(bottom: 25)),
+          CircularProgressIndicator()
+        ],
+      ),
+    );
+  }
+
+  Widget rentsList() {
+    return GridView.builder(
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: _rents.length,
         itemBuilder: (context, index) {
-          Rent rent = rents[index];
+          Rent rent = _rents[index];
           return Card(
-           // color: Colors.pinkAccent,
+            // color: Colors.pinkAccent,
             child: Container(
               padding: const EdgeInsets.fromLTRB(15, 10, 10, 0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Expanded(child: rent.image),
+                  Expanded(child: Image.network(rent.image)),
                   const Divider(color: Colors.grey),
                   ListTile(
-                  title: Text(
-                    rent.title,
-                    style: const TextStyle(
+                    title: Text(
+                      rent.title,
+                      style: const TextStyle(
                         color: Colors.grey,
                         fontWeight: FontWeight.bold,
                         fontSize: 13,
+                      ),
+                      textAlign: TextAlign.start,
                     ),
-                    textAlign: TextAlign.start,
-                  ),
-                  subtitle: Text(
-                    "price/month: ${rent.price_per_month}£",
-                    style: const TextStyle(color: Colors.grey,fontSize: 11),
-                  ),
-                    onTap:  () {
+                    subtitle: Text(
+                      "price/month: \$${rent.price_per_month.toString()}£",
+                      style: const TextStyle(color: Colors.grey, fontSize: 11),
+                    ),
+                    onTap: () {
                       showDialog(
                           context: context,
                           builder: (BuildContext context) {
@@ -56,25 +101,22 @@ class _RentPageState extends State<RentPage> {
                                 "Location : ${rent.location}\n"
                                 "Room number : ${rent.room_number}\n"
                                 "Size : ${rent.size} sqft\n"
-                                 "Availability :\n"
-                                    " ${rent.start_date_available.year}-${rent.start_date_available.month}-${rent.start_date_available.day}"
-                                    " to ${rent.end_date_available.year}-${rent.end_date_available.month}-${rent.end_date_available.day}"
-                                    "\n Price/month: ${rent.price_per_month}£",
+                                "Availability :\n"
+                                " ${rent.start_date_available}"
+                                " to ${rent.end_date_available}"
+                                "\n Price/month: \$${rent.price_per_month.toString()}£",
                                 style: const TextStyle(
-                                  fontSize: 15,
-                                    color: Colors.grey),
+                                    fontSize: 15, color: Colors.grey),
                               ),
-                              content: rent.image,
+                              content: Image.network(rent.image),
                             );
-                          }
-                      );
+                          });
                     },
                   ),
                 ],
               ),
             ),
           );
-        }
-    ),
-  );
+        });
+  }
 }
