e91b4437e4b0fd5300a02f9d5e7b9e9217869ccb
yousseff650 <yousseffarhat818@gmail.com>
2021-12-30 17:15:31 +0100

    Crud About (agency)

--- Frontend/Angular-Real-Estate-Project--master/src/app/about/about-routing.module.ts
+++ Frontend/Angular-Real-Estate-Project--master/src/app/about/about-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about.component';
 import { AboutDetailsComponent } from './about-details/about-details.component';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 const routes: Routes = [
   { path: '', component: AboutComponent },
   { path: ':id', component: AboutDetailsComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  imports: [RouterModule.forChild(routes), 
+    ReactiveFormsModule,
+    FormsModule],
+  exports: [RouterModule, ReactiveFormsModule,FormsModule],
   declarations: [
     AboutDetailsComponent
   ]
--- Frontend/Angular-Real-Estate-Project--master/src/app/about/about.component.ts
+++ Frontend/Angular-Real-Estate-Project--master/src/app/about/about.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Agencys } from '../about-list';
 import { Agency } from '../about.model';
 
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
 import { AboutService } from '../services/about.service';
+import { aboutmodel } from './aboutmodel.model';
 
 @Component({
   selector: 'app-about',
@@ -12,13 +16,72 @@ import { AboutService } from '../services/about.service';
 export class AboutComponent implements OnInit {
 
    public agencyList: any =[];
-
+   public formValue: FormGroup;
+   aboutmodel: aboutmodel = new aboutmodel();
   constructor(
-    private aboutService: AboutService
-  ) { }
+    private aboutService: AboutService,
+    private formBuilder: FormBuilder,
+    public router: Router
+  ) {this.formValue = this.formBuilder.group({
+    owner: [''],
+    image_owner: [''],
+    mail: [''],
+    phone: [''],
+    location: [''],
+    image_location:[''],
+    siege_social: [''],
+    date_creation: [''],
+    horaire: [''],
+    description: [''],
+  }); }
 
   ngOnInit(): void {
+    this.getAllAbouts();
+  }
+  public getAllAbouts(){
     this.aboutService.all().subscribe((res) => (this.agencyList = res));;
   }
 
+  
+  public addAbout() {
+    this.aboutService.create(this.formValue.value).subscribe((res) => {
+      if (res.status == 201) {
+        this.formValue.reset();
+        alert('Agency added successfully');
+        this.getAllAbouts();
+      }
+    });
+  }
+
+
+  onEditAbout(about: any) {
+    this.aboutmodel._id = about._id;
+    this.formValue.controls['owner'].setValue(about.owner);
+    this.formValue.controls['image_owner'].setValue(about.image_owner);
+    this.formValue.controls['mail'].setValue(about.mail);
+    this.formValue.controls['phone'].setValue(about.phone);
+    this.formValue.controls['location'].setValue(about.location);
+    this.formValue.controls['image_location'].setValue(about.image_location);
+    this.formValue.controls['siege_social'].setValue(about.siege_social);
+    this.formValue.controls['date_creation'].setValue(about.date_creation);
+    this.formValue.controls['horaire'].setValue(about.horaire);
+    this.formValue.controls['description'].setValue(about.description);
+  }
+  UpdateAbout() {
+    this.aboutService
+      .update(this.aboutmodel._id, this.formValue.value)
+      .subscribe((data) => {
+        alert('Agency updated successfully');
+        this.formValue.reset();
+        this.router.navigate(['/about']);
+        window.location.reload();
+      });
+  }
+
+  public delete(about: any) {
+    this.aboutService.delete(about._id).subscribe((res) => {
+      alert('Agency deleted ');
+      this.getAllAbouts();
+    });
+  }
 }
