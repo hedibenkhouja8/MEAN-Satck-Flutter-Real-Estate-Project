285a1205ab6ccdf903aaf7be3e03e493ebd11284
oussama-abid <oussamaabid52@gmail.com>
2021-12-31 02:55:35 +0100

    register works

--- Frontend/Angular-Real-Estate-Project--master/src/app/auth/auth-routing.module.ts
+++ Frontend/Angular-Real-Estate-Project--master/src/app/auth/auth-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
@@ -11,7 +11,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    FormsModule
+  ],
   exports: [RouterModule],
   declarations: [
     LoginComponent,
