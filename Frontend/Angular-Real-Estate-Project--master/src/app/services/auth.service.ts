4d99bf50bc35fac69f905f9ab2e8a5c10525e6c9
yousseff650 <yousseffarhat818@gmail.com>
2021-12-31 13:39:25 +0100

    login backend

--- Frontend/Angular-Real-Estate-Project--master/src/app/services/auth.service.ts
+++ Frontend/Angular-Real-Estate-Project--master/src/app/services/auth.service.ts
@@ -1,23 +1,30 @@
 import { Injectable } from '@angular/core';
 
 import { HttpHeaders, HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+
+import { Router } from "@angular/router";
+import { LocalStorageService } from "src/app/services/local-storage.service";
 
 import { User } from 'src/app/user.model';
 
 @Injectable({
   providedIn: 'root'
-})
-export class AuthService {
+})export class AuthService {
 
   API_URL: string = 'http://localhost:3000/api';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
+  public isLoggedIn:boolean = false;
 
   constructor(
-    private httpClient: HttpClient
-  ) { }
+    private httpClient: HttpClient,
+    public router: Router,
+    private localStorageService: LocalStorageService
+  ) {
+    this.isLoggedIn = this.getAccessToken() ? true : false;
+   }
 
   signup(user: User): Observable<any> {
 
@@ -26,15 +33,51 @@ export class AuthService {
     )
   }
 
+  login(user: User) {
+    return this.httpClient.post<any>(`${this.API_URL}/auth/login`, user)
+      .subscribe((res: any) => {
+        this.isLoggedIn = true;
+        this.localStorageService.set('access_token', res.token);
+        this.router.navigate(['customer/profile/' + res.userId]);
+        // this.getUserProfile(res.userId).subscribe((res) => {
+        //   this.localStorageService.set('user', {email: res.email, id: res._id, name: res.name});
+        // })
+      })
+  }
+
+  getUserProfile(id: string): Observable<any> {
+    return this.httpClient.get(`${this.API_URL}/auth/profile/${id}`, { headers: this.headers }).pipe(
+      map((res: Object) => {
+        return res || {}
+      }),
+      catchError(this.handleError)
+    )
+  }
+
   handleError(error: HttpErrorResponse) {
-    let msg = '';
+    let err;
     if (error.error instanceof ErrorEvent) {
       // client-side error
-      msg = error.error.message;
+      err = {message: error.error.message};
     } else {
       // server-side error
-      msg = `Error Code: ${error.status}\nMessage: ${error.message}`;
+      err = { code: error.status, message: error.message };
     }
-    return throwError(msg);
+    return throwError(err);
+  }
+
+  getAccessToken() {
+    return this.localStorageService.get('access_token');
+  }
+
+  getUserId() {
+    return this.localStorageService.get('user').id;
+  }
+
+  logout() {
+    this.localStorageService.set('access_token', null);
+    this.localStorageService.set('user', null);
+    this.isLoggedIn = false;
+    this.router.navigate(['/']);
   }
 }
