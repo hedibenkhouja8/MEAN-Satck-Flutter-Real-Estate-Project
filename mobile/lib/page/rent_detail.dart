f4daadc6cacfdc8334b55ad31bca4dc0076f431e
labidimalek <melek.islem105@gmail.com>
2022-01-03 23:26:58 +0100

    delete button fixed

--- mobile/lib/page/rent_detail.dart
+++ mobile/lib/page/rent_detail.dart
@@ -7,8 +7,9 @@ class RentDetail extends StatelessWidget {
   final rent;
 
   RentDetail({
+    Key? key,
     required this.rent,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +19,7 @@ class RentDetail extends StatelessWidget {
         elevation: 0.0,
         centerTitle: true,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Color(0xFF545D68)),
+          icon: const Icon(Icons.arrow_back, color: Color(0xFF545D68)),
           onPressed: () {
             Navigator.of(context).pop();
           },
@@ -32,15 +33,15 @@ class RentDetail extends StatelessWidget {
       body: ListView(children: [
         const SizedBox(height: 15.0),
         Padding(
-          padding: EdgeInsets.only(left: 20.0),
+          padding: const EdgeInsets.only(left: 20.0),
           child: Text(rent.title,
-              style: TextStyle(
+              style: const TextStyle(
                   fontFamily: 'Varela',
                   fontSize: 42.0,
                   fontWeight: FontWeight.bold,
                   color: Color.fromRGBO(212, 202, 104, 1))),
         ),
-        SizedBox(height: 15.0),
+        const SizedBox(height: 15.0),
         ClipRRect(
             borderRadius: const BorderRadius.only(
               topLeft: Radius.circular(10.0),
@@ -48,65 +49,78 @@ class RentDetail extends StatelessWidget {
             ),
             child: Image.asset(rent.image,
                 height: 150.0, width: 100.0, fit: BoxFit.contain)),
-        SizedBox(height: 20.0),
+        const SizedBox(height: 20.0),
         Center(
           child: Text("£${rent.price_per_month}/Mo",
-              style: TextStyle(
+              style: const TextStyle(
                   fontFamily: 'Varela',
                   fontSize: 22.0,
                   fontWeight: FontWeight.bold,
                   color: Color.fromRGBO(212, 202, 104, 1))),
         ),
-        SizedBox(height: 10.0),
+        const SizedBox(height: 10.0),
         Center(
           child: Text(rent.location,
-              style: TextStyle(
+              style: const TextStyle(
                   color: Color(0xFF575E67),
                   fontFamily: 'Varela',
                   fontSize: 20.0)),
         ),
-        SizedBox(height: 10.0),
+        const SizedBox(height: 10.0),
         Center(
           child: Text(
               "${rent.start_date_available} to ${rent.end_date_available} ",
-              style: TextStyle(
+              style: const TextStyle(
                   color: Color(0xFF575E67),
                   fontFamily: 'Varela',
                   fontSize: 20.0)),
         ),
-        SizedBox(height: 10.0),
+        const SizedBox(height: 10.0),
         Center(
           child: Text("${rent.owner}",
-              style: TextStyle(
+              style: const TextStyle(
                   color: Color(0xFF575E67),
                   fontFamily: 'Varela',
                   fontSize: 20.0)),
         ),
-        SizedBox(height: 20.0),
+        const SizedBox(height: 20.0),
         Center(
           child: Container(
             width: MediaQuery.of(context).size.width - 50.0,
             child: Text(rent.description,
                 textAlign: TextAlign.start,
-                style: TextStyle(
+                style: const TextStyle(
                     fontFamily: 'Varela',
                     fontSize: 16.0,
                     color: Color(0xFFB4B8B9))),
           ),
         ),
-        FlatButton(
-            padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-            child: const Text(
-              'Delete',
-              style: TextStyle(
-                  fontSize: 13,
-                  color: Color.fromRGBO(800, 114, 178, 1),
-                  fontWeight: FontWeight.normal),
-            ),
-            onPressed: () {
-              delete(rent.id);
-              Navigator.pushNamed(context, '/');
-            })
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FlatButton(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                child: Row(
+                  children: const [
+                    Text(
+                      'Delete ',
+                      style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.redAccent,
+                          fontWeight: FontWeight.normal),
+                    ),
+                    Icon(
+                      Icons.delete,
+                      color: Colors.redAccent,
+                    ),
+                  ],
+                ),
+                onPressed: () {
+                  delete(rent.id);
+                  Navigator.pushNamed(context, '/');
+                })
+          ],
+        ),
       ]),
     );
   }
