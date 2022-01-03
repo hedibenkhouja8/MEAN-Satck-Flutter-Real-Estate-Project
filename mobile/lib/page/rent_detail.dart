8e26bfb5b48f883aa41d272a5819e798e6c52dd1
oussama-abid <oussamaabid52@gmail.com>
2022-01-03 22:29:36 +0100

    rent delete

--- mobile/lib/page/rent_detail.dart
+++ mobile/lib/page/rent_detail.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/cupertino.dart';
+import 'dart:convert';
 
 class RentDetail extends StatelessWidget {
   final rent;
 
-  RentDetail(
-      {required this.rent,
-   });
+  RentDetail({
+    required this.rent,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +31,7 @@ class RentDetail extends StatelessWidget {
       ),
       body: ListView(children: [
         const SizedBox(height: 15.0),
-         Padding(
+        Padding(
           padding: EdgeInsets.only(left: 20.0),
           child: Text(rent.title,
               style: TextStyle(
@@ -38,11 +41,12 @@ class RentDetail extends StatelessWidget {
                   color: Color.fromRGBO(212, 202, 104, 1))),
         ),
         SizedBox(height: 15.0),
-       ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(10.0),
-                  topRight: Radius.circular(8.0),
-                ), child :Image.asset(rent.image,
+        ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(10.0),
+              topRight: Radius.circular(8.0),
+            ),
+            child: Image.asset(rent.image,
                 height: 150.0, width: 100.0, fit: BoxFit.contain)),
         SizedBox(height: 20.0),
         Center(
@@ -63,7 +67,8 @@ class RentDetail extends StatelessWidget {
         ),
         SizedBox(height: 10.0),
         Center(
-          child: Text("${rent.start_date_available} to ${rent.end_date_available} ",
+          child: Text(
+              "${rent.start_date_available} to ${rent.end_date_available} ",
               style: TextStyle(
                   color: Color(0xFF575E67),
                   fontFamily: 'Varela',
@@ -89,7 +94,29 @@ class RentDetail extends StatelessWidget {
                     color: Color(0xFFB4B8B9))),
           ),
         ),
+        FlatButton(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+            child: const Text(
+              'Delete',
+              style: TextStyle(
+                  fontSize: 13,
+                  color: Color.fromRGBO(800, 114, 178, 1),
+                  fontWeight: FontWeight.normal),
+            ),
+            onPressed: () {
+              delete(rent.id);
+              Navigator.pushNamed(context, '/');
+            })
       ]),
     );
+  }
+
+  Future delete(String id) async {
+    String url = 'http://localhost:3000/rents/$id';
+    final deleteurl = (Uri.parse(url));
+    final response = await http.delete(deleteurl);
+    print(response.statusCode);
+    print(response.body);
+    return json.decode(response.body);
   }
 }
